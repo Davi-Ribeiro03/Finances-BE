38b4dc176fdc6f13967116806a88a9a12edacda5
Davi Ribeiro <davi.75.ef@gmail.com>
2026-02-26 15:00:24 -0300

    initial setup

---
+++ src/test/java/com/davi/finances/FinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
