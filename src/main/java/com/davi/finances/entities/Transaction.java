218430eff29c80c6f7685ef4d767cf05dd91dc82
Davi Ribeiro <davi.75.ef@gmail.com>
2026-02-27 14:43:35 -0300

    Adds entities transaction and user

---
+++ src/main/java/com/davi/finances/entities/Transaction.java
@@ -0,0 +1,29 @@
+package com.davi.finances.entities;
+
+import com.davi.finances.enums.Categories;
+import com.davi.finances.enums.transactionsType;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Transaction {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private BigDecimal value;
+    private Date date;
+    private String description;
+    private Categories category;
+    private String month_reference;
+    @Enumerated(EnumType.STRING)
+    private transactionsType type;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+}
