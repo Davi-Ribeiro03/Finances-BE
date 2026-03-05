6d149bfee8d6ed03aa76e7240c714c0a29f153c9
Davi Ribeiro <davi.75.ef@gmail.com>
2026-03-05 17:46:59 -0300

    Implements the response pattern and the global exception handler

---
+++ src/main/java/com/davi/finances/configs/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.davi.finances.configs;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handleException(Exception ex){
+
+        ApiResponse<Object> response =
+                new ApiResponse<>(false, ex.getMessage(), null);
+
+        return ResponseEntity.internalServerError().body(response);
+    }
+}
